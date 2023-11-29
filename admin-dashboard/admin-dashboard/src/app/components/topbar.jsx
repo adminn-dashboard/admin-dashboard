7d6af609fabe426b7df3c8344407959de41e474d
Toluwalase Ogunsola <93495428+Captain-awesome619@users.noreply.github.com>
2023-11-29 08:40:55 +0100

    fixed postion

--- admin-dashboard/admin-dashboard/src/app/components/topbar.jsx
+++ admin-dashboard/admin-dashboard/src/app/components/topbar.jsx
@@ -3,7 +3,7 @@ import { IoMdNotificationsOutline } from "react-icons/io";
 export const Topbar = () => {
   return (
     <div className=" ">
-        <div className="absolute  bg-gray-800 border-[1px] border-gray-300  w-full  h-max  ">
+        <div className="fixed  bg-gray-800 border-[1px] border-gray-300  w-full  h-max  ">
 <div className="flex flex-row items-center justify-between py-[0.5rem] pl-[1rem]">
 <input placeholder="Search" className="rounded-md w-[450px]  h-[45px] bg-gray-500  pl-[0.5rem] text-white placeholder-white"/>
 <div className="flex flex-row items-center justify-center relative right-[25%]  gap-4">
--- admin-dashboard/admin-dashboard/src/app/dashboard/page.jsx
+++ admin-dashboard/admin-dashboard/src/app/dashboard/page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const pages = () => {
   return (
-    <div>dashboard</div>
+    <div className="text-white z-10">dashboard</div>
   )
 }
 
