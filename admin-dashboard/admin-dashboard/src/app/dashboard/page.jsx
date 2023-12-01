e47b465754335796ab23febefe94441472de6e2a
heywhyrex <oyeniyan2000@gmail.com>
2023-12-01 04:12:28 +0100

    dashboard and stats

--- admin-dashboard/admin-dashboard/src/app/dashboard/page.jsx
+++ admin-dashboard/admin-dashboard/src/app/dashboard/page.jsx
@@ -4,7 +4,7 @@ import PendingApprovalShorts from './dashboardComponents/PendingApprovalShorts';
 import PendingApprovalPhotos from './dashboardComponents/PendingApprovalPhotos';
 const pages = () => {
   return (
-    <div className='bgset h-max w-full z-10 text-white'>
+    <div className='bgset h-max w-full  text-white'>
       <div className='w-11/12 m-auto mt-10'>
         <div>
           <h1 className='text-2xl font-semibold'>Main Dashboard</h1>
--- admin-dashboard/admin-dashboard/src/app/layout.js
+++ admin-dashboard/admin-dashboard/src/app/layout.js
@@ -1,26 +1,35 @@
-"use client"
-import { Inter } from 'next/font/google'
-import './globals.css'
-import Sidebar from './components/sidebar/sidebar'
-import { useSelectedLayoutSegment } from 'next/navigation'
-import { Topbar } from './components/topbar'
-const inter = Inter({ subsets: ['latin'] })
+'use client';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import Sidebar from './components/sidebar/sidebar';
+import { useSelectedLayoutSegment } from 'next/navigation';
+import { Topbar } from './components/topbar';
+const inter = Inter({ subsets: ['latin'] });
 export default function RootLayout({ children }) {
-  const segment = useSelectedLayoutSegment()
-console.log( `/${segment}` )
+  const segment = useSelectedLayoutSegment();
+  console.log(`/${segment}`);
   return (
-    <html lang="en">
-      <body  className="flex flex-wrap  mx-auto w-full bg-black  overflow-y-auto ">
-
-      {`/${segment}` === "/null" || `/${segment}` === "/forgot-password" ? "" : <div className="fixed flex flex-row"><Sidebar/>
-<Topbar/>
-      </div>}
-
-<div className={`/${segment}` === "/null" || `/${segment}` === "/forgot-password" ? "flex  w-full h-[100%]" : " overflow-x-hidden overflow-y-auto pt-[5%] pl-[10%]  w-full flex  justify-center " }>
-  {children}
-</div>
-
-        </body>
+    <html lang='en'>
+      <body className='flex flex-wrap  mx-auto w-full bg-black  overflow-y-auto '>
+        {`/${segment}` === '/null' || `/${segment}` === '/forgot-password' ? (
+          ''
+        ) : (
+          <div className='fixed flex flex-row'>
+            <Sidebar />
+            <div>
+              <Topbar />
+            </div>
+          </div>
+        )}
+        <div
+          className={
+            `/${segment}` === '/null' || `/${segment}` === '/forgot-password'
+              ? 'flex  w-full h-[100%]'
+              : ' overflow-x-hidden overflow-y-auto pt-[5%] pl-[10%]  w-full flex  justify-center '
+          }>
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
